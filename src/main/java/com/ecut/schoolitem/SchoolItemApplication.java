59716e5ac0f6e8cd3ca4e469f06ddf6adf7bc23b
qiukai <3052844096@qq.com>
2025-12-15 14:11:23 +0800

    Initial commit

---
+++ src/main/java/com/ecut/schoolitem/SchoolItemApplication.java
@@ -0,0 +1,13 @@
+package com.ecut.schoolitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolItemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecut/schoolitem/SchoolItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecut.schoolitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolItemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
